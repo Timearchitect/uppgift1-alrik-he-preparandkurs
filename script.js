147743954d2f02bd5650b1dc86afc6657dfde959
Alrik He <alrik_he@hotmail.com>
2024-11-18 18:36:13 +0100

    first

---
+++ script.js
@@ -0,0 +1,34 @@
+/*
+alert(123)
+document.write("<h2> TJENARE </h2>")
+console.log(789)
+*/
+
+// console.log(789)
+
+
+// 1) Variable  koncept behållare 
+
+let alrik   // skapar en variabel
+alrik = 5   // skriver över in ett värde, nummer 5
+alrik = 10
+alrik = 10 + 10
+alrik = (2 + 3) * 2
+alrik = "hej" + "san"  // konkatination klistrar ihop text
+alrik = 1 + 1          // kalkylation
+alrik = 1 + 1 + "hej"  // "2hej"
+alrik = "hej1" + (1 + 1)  // "hej11"
+
+//P arentess
+//E xponent
+//M ultiplikation
+//D ivition
+//A ddition
+//S ubtraktion
+
+let filip = "hej"  // deklarera en variabel & tilldela med strängen "hej"
+
+console.log(alrik)
+console.log(filip)
+console.log(alrik+filip)
+
